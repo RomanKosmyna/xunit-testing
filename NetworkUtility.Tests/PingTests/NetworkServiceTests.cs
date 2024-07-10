2bfb5da747e005023f2c1a3a43db2ae5fbc6ba90
RomanKosmyna <roma.kosmyna@gmail.com>
2024-07-10 18:21:59 +0300

    Initial commit

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange
+            var pingService = new NetworkService();
+
+            // Act
+            var result = pingService.SendPing();
+
+            // Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            //result.Should().Contain("Ping", Exactly.Once());
+        }
+    }
+}
